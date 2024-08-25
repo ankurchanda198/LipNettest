4862982308f11886d50b7d0c9ae5373bf5d229e7
Ankur <ankurchanda198@gmail.com>
2024-08-25 14:24:30 +0530

    added app

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ st.set_page_config(layout='wide')
 # Setup the sidebar
 with st.sidebar: 
     st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
-    st.title('LipBuddy')
+    st.title('LipNet Full Stack App')
     st.info('This application is originally developed from the LipNet deep learning model.')
 
 st.title('LipNet Full Stack App') 
